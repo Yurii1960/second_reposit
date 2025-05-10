b47275a3b242bbe5ea498eca0189cc9f2fff02f1
Yurii1960 <jura.ntu.most@gmail.com>
2025-05-10 21:57:28 +0300

    ДЗ 3-3

--- main3.py
+++ main3.py
@@ -1,20 +1,24 @@
 import re
-phone_number="    +38 (050)A789988"
-pattern=r"\d+"
-normal_tel=re.findall(pattern,phone_number)
-print(normal_tel)
-result="".join(normal_tel)
-if len(result)==12:
-    telef="+ " +result
-    print(telef)
-elif len(result)==10:
-    telef="+38  " +result
-    print(telef)
+def normalize_phone(phone_number):
+
+    pattern=r"\d+"
+    normal_tel=re.findall(pattern,phone_number)
+    
+    result="".join(normal_tel)
+    if len(result)==12:
+        telef="+ " +result
+        
+    elif len(result)==10:
+        telef="+38 " +result
+   
 
 
-else:
-      print("Перевірте номер телефону")
+    else:
+        print("Перевірте номер телефону")
+    
+    return telef
 
 
 
-print(result,len(result))
+
+
