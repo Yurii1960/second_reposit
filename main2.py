081154c2eea2f80038afd47dc84786b1e74cdd81
Yurii1960 <jura.ntu.most@gmail.com>
2025-05-13 18:26:35 +0300

    ДЗ 3-2 змінено 13.05

--- main2.py
+++ main2.py
@@ -2,12 +2,21 @@ import random
 
 
 def get_numbers_ticket(min,max,quantity):
+    if min<=0:
+        print("мінімальне значення повинно бути більшим нуля")
+    elif quantity<=0: 
+        print("Кількість номерів повинна бути більшим нуля")
+    elif max<=min+quantity:
+        print(f"максимальне значення повинно бути більшим {min+quantity}")
+    else:
 
-    num=[]
-    for i in range(min,max+1):
-        num.append(i)
-    numbers=random.sample(num,quantity)
-    numbers.sort()
-    return numbers
+       
 
-print(f"Ваші лоторейні числа : {get_numbers_ticket(1,49,6)}")
+        num=[]
+        for i in range(min,max+1):
+            num.append(i)
+        numbers=random.sample(num,quantity)
+        numbers.sort()
+        return numbers
+
+
