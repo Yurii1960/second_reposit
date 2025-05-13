f910823df808b1d72e4485bf0b68caa067d2b741
Yurii1960 <jura.ntu.most@gmail.com>
2025-05-13 17:48:00 +0300

    ДЗ 3-1 версія13.05

--- main1.py
+++ main1.py
@@ -5,25 +5,25 @@ from datetime import *
 
     
 def get_days_from_today(date):
-     while True:
-        date=input('Введіть дату в форматі :YYYY-MM-DD   ')
+     
+    try:
 
 
-        data_time=datetime.strptime(date, '%Y-%m-%d')
-        today=datetime.today()
-        difference_days=(today-data_time)
+       data_time=datetime.strptime(date, '%Y-%m-%d')
+     
+       today=datetime.today()
+       difference_days=(today-data_time)
     
         
-        try:
-           result=int(difference_days.days)
+        
+       result=difference_days.days
 
-           print(f"До вашої дати залишилось = {-result} дні ")
-           return int(difference_days.days)
+       print(f"До вашої дати залишилось  {-result} дні ")
+       return difference_days.days
+    except ValueError:
+         print("Введіть дату згідно формату YYYY-MM-DD   ")  
 
-           break
-        except ValueError:
-            print("Введіть правильно дату    ")    
-
+     
 
 
                           
