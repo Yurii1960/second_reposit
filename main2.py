b814d9654d1cb517e19f20f1ee3646b26decb568
Yurii1960 <jura.ntu.most@gmail.com>
2025-05-14 11:08:51 +0300

    ДЗ 3-2 (14.05)

--- main2.py
+++ main2.py
@@ -1,26 +1,28 @@
 import random
 
-
 def get_numbers_ticket(min,max,quantity):
     if min<=0:
         print("мінімальне значення повинно бути більшим нуля")
+        return []
     elif quantity<=0: 
         print("Кількість номерів повинна бути більшим нуля")
-    elif max<=min+quantity :
+        return []
+
+    elif max<min+quantity :
 
         print(f"максимальне значення повинно бути більшим {min+quantity}")
+        return []
     elif max>1000:
+        
         print("Максимальне значення повинно бути менше 1000")
+        return []
     else:
-
-       
-
         num=[]
         for i in range(min,max+1):
             num.append(i)
         numbers=random.sample(num,quantity)
         numbers.sort()
         return numbers
-print(get_numbers_ticket(-100,1000,10))
+print(get_numbers_ticket(10,1005,3))
                          
 
